db33291a96393824bf7294a66f84ecffb34aa35a
NikhilPIO <89526041+NikhilPIO@users.noreply.github.com>
2022-06-30 12:52:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const path = require('path');
 const bodyParser = require('body-parser');
 var fs = require('fs');
 
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.use(express.static('static'));
 //to get product data
-app.get('/getUsers', function(req, res){
+app.get('/getProd', function(req, res){
   fs.readFile(__dirname + "/" + "db.json", 'utf8', function(err, data){
       console.log(data);
       res.end(data); 
@@ -18,29 +20,38 @@ app.get('/getUsers', function(req, res){
 })
 
 var prod = {
-    "prod5": {
-    "id":5,
-    "name":"Shirt",
-    "Cost":200,
-    "Desc":"US Polo"
-    }
-} 
+  "prod5": {
+  "id":5,
+  "name":"Shirt",
+  "Cost":200,
+  "Desc":"US Polo"
+  }
+};
 
-//addUser endpoint
-app.post('/addUser', function(req, res){
+//add product endpoint
+app.get('/addProd', function(req, res){
   //Step 2: read existing users
-  fs.readFile(__dirname + "/" + "db.json", 'utf8', function(err, data){
+    fs.readFile(__dirname + "/" + "db.json", 'utf8', function(err, data){
       data = JSON.parse(data);
-      //Step 3: append user variable to list
       data["prod5"] = prod["prod5"];
-      console.log(data);
-      res.end(JSON.stringify(data));
-  });
+      //Step 3: append user variable to list
+      var jsonContent = JSON.stringify(data);
+      
+      fs.writeFile("db.json", jsonContent, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+     
+        console.log("JSON file has been saved.");
+    });
+});
 })
 
-//delete a user by id
+
+//delete a product by id
 var id = 3;
-app.delete('/deleteUser', function (req, res) {
+app.get('/deleteProd', function (req, res) {
    // First retrieve existing users
    fs.readFile( __dirname + "/" + "db.json", 'utf8', function (err, data) {
       data = JSON.parse( data );
@@ -52,8 +63,23 @@ app.delete('/deleteUser', function (req, res) {
 })
 
 
+app.get('/UpdateProd', function (req, res) {
+  const fileName = './db.json';
+  const file = require(fileName);
+  file.prod4.Cost = 350; 
+  
+  fs.writeFile("db.json", JSON.stringify(file), function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file));
+    console.log('writing to ' + fileName);
+  });
+
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
 
